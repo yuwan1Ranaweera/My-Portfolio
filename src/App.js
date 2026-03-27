66dab1fb530b4a7e6be5b11c7e78b9af4076ba74
Yuwani <yuwaniranaweera@gmail.com>
2026-03-27 19:22:23 +0530

    Create app.js

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Hero from './components/Hero';
+import About from './components/About';
+import Skills from './components/Skills';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+
+function App() {
+  return (
+    <div>
+      <Hero />
+      <About />
+      <Skills />
+      <Projects/>
+      <Contact/>
+    </div>
+  );
+}
+
+export default App;
